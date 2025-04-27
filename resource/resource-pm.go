5efadced71fbf1acbd61b2f3533e706345c72563
Matthew Gabeler-Lee <cheetah@fastcat.org>
2025-04-27 12:00:47 -0400

    fix: flesh out pm resource

--- resource/resource-pm.go
+++ resource/resource-pm.go
@@ -3,6 +3,7 @@ package resource
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"fastcat.org/go/gdev/pm/api"
 )
@@ -63,10 +64,63 @@ func (p *PM) Start(ctx *Context) error {
 
 // Stop implements Resource.
 func (p *PM) Stop(ctx *Context) error {
-	panic("unimplemented")
+	client := ContextValue[api.API](ctx)
+	child, err := p.Config(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get child config: %w", err)
+	}
+	cur, err := client.Child(ctx, child.Name)
+	// loop until we can stop and remove the child
+	retry := time.NewTicker(10 * time.Millisecond)
+	defer retry.Stop()
+	for {
+		if err != nil {
+			if api.IsNotFound(err) {
+				// not defined => definitely stopped
+				return nil
+			}
+			return fmt.Errorf("failed checking child %s status: %w", child.Name, err)
+		}
+		switch cur.Status.State {
+		case api.ChildStopped:
+			cur, err = client.DeleteChild(ctx, child.Name)
+			// check cur/err again at the top
+		case api.ChildError, api.ChildInitError, api.ChildInitRunning, api.ChildRunning:
+			cur, err = client.StopChild(ctx, child.Name)
+		case api.ChildStopping:
+			// wait
+			select {
+			case <-ctx.Done():
+				return context.Cause(ctx)
+			case <-retry.C:
+				// refresh and loop back to check status again
+				cur, err = client.Child(ctx, child.Name)
+			}
+		default:
+			return fmt.Errorf("child %s: unrecognized state %q", child.Name, cur.Status.State)
+		}
+	}
 }
 
 // Ready implements Resource
 func (p *PM) Ready(ctx *Context) (bool, error) {
-	panic("unimplemented")
+	client := ContextValue[api.API](ctx)
+	child, err := p.Config(ctx)
+	if err != nil {
+		return false, fmt.Errorf("failed to get child config: %w", err)
+	}
+	cur, err := client.Child(ctx, child.Name)
+	if err != nil {
+		return false, fmt.Errorf("failed checking child %s status: %w", child.Name, err)
+	}
+	if cur.Status.State != api.ChildRunning {
+		// TODO: Done state for one-shot jobs
+		// TODO: say why it's unhealthy
+		return false, nil
+	}
+	if child.HealthCheck == nil {
+		// TODO: wait for it to run for a min amount of time?
+		return true, nil
+	}
+	return cur.Status.Health.Healthy, nil
 }
