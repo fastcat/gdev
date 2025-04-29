874347fd242b2402fe9d98100db4e3d8812a85b3
Matthew Gabeler-Lee <cheetah@fastcat.org>
2025-04-29 18:17:27 -0400

    fix: don't delete pvcs

--- addons/k8s/resource-storage.go
+++ addons/k8s/resource-storage.go
@@ -4,6 +4,8 @@ import (
 	apiCoreV1 "k8s.io/api/core/v1"
 	applyCoreV1 "k8s.io/client-go/applyconfigurations/core/v1"
 	clientCoreV1 "k8s.io/client-go/kubernetes/typed/core/v1"
+
+	"fastcat.org/go/gdev/resource"
 )
 
 type pvc struct {
@@ -12,6 +14,12 @@ type pvc struct {
 		apiCoreV1.PersistentVolumeClaim,
 		*applyCoreV1.PersistentVolumeClaimApplyConfiguration,
 	]
+}
+
+func (r *pvc) Stop(ctx *resource.Context) error {
+	// deleting PVCs will generally result in deleting storage, which we do not
+	// want (e.g. deleting DB data). This is therefore a no-op.
+	return nil
 }
 
 func PersistentVolumeClaim(apply *applyCoreV1.PersistentVolumeClaimApplyConfiguration) Resource {
