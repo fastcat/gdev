1144256b6ea112a6032fe72dc2b13a70a5f2be20
Matthew Gabeler-Lee <cheetah@fastcat.org>
2025-04-29 18:33:16 -0400

    fix: allow appname lockdown earlier

--- internal/app-name.go
+++ internal/app-name.go
@@ -3,20 +3,29 @@ package internal
 import (
 	"fmt"
 	"strings"
+	"sync/atomic"
 	"unicode"
 )
 
 var appName = "gdev"
 
+// we need to be able to access the app name early in a lot of places, so it has
+// its own lockdown tracker in addition to the main one
+var appNameLocked atomic.Bool
+
 // AppName is what the app will call itself. When customizing, overwrite it
 // before calling Main().
 func AppName() string {
-	CheckLockedDown()
+	// once observed it cannot be changed
+	appNameLocked.Store(true)
 	return appName
 }
 
 func SetAppName(name string) {
 	CheckCanCustomize()
+	if appNameLocked.Load() {
+		panic(fmt.Errorf("app name is locked"))
+	}
 	if name == "" {
 		panic(fmt.Errorf("app name must not be empty"))
 	}
