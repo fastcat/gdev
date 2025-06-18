caa44782118f9dc2bc3a0388eb84d5fe5bf978d1
Matthew Gabeler-Lee <cheetah@fastcat.org>
2025-06-18 16:49:10 -0400

    fix: add ko build config for sample stack services, run in ci

---
+++ examples/stack/svc1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+// this is a dumb service that just runs a hello-world style http server
+func main() {
+	http.HandleFunc("GET /{$}", http.HandlerFunc(getRoot))
+	addr := os.Getenv("ADDR")
+	if addr == "" {
+		addr = ":8080" // default address
+	}
+	fmt.Println("Starting server on", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		if err != http.ErrServerClosed {
+			panic(err) // panic if the server fails to start
+		}
+	}
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("Hello, World!"))
+}
