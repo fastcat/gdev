df4a0807dc1cffbac84ad9f5b9c700de21dc3444
Matthew Gabeler-Lee <cheetah@fastcat.org>
2025-07-21 18:00:14 -0400

    fix: more helpful build targets

--- magefiles/compile.go
+++ magefiles/compile.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"path/filepath"
 
 	"github.com/magefile/mage/mg"
 
@@ -29,18 +30,41 @@ func Compile(ctx context.Context) error {
 type Build mg.Namespace
 
 func (Build) Debug(ctx context.Context) error {
-	fmt.Println("Build gdev debug binary")
-	return shx.Run(ctx, "go", "build", "-gcflags=-N -l", "-v", "-o", "./gdev.debug", "./examples/gdev")
+	mg.CtxDeps(ctx, mg.F(Build{}.debug, "./examples/gdev", "./gdev.debug"))
+	return nil
 }
 
 func (Build) Release(ctx context.Context) error {
-	fmt.Println("Build gdev release binary")
+	mg.CtxDeps(ctx, mg.F(Build{}.release, "./examples/gdev", "./gdev"))
+	return nil
+}
+
+func (Build) debug(ctx context.Context, pkg, name string) error {
+	fmt.Printf("Build %s debug binary\n", filepath.Base(pkg))
 	return shx.Cmd(
 		ctx,
-		"go", "build", "-ldflags=-s -w", "-v", "-o", "./gdev", "./examples/gdev",
+		"go", "build", "-gcflags=-N -l", "-v", "-o", name, pkg,
+	).Run()
+}
+
+func (Build) release(ctx context.Context, pkg, name string) error {
+	fmt.Printf("Build %s release binary\n", filepath.Base(pkg))
+	return shx.Cmd(
+		ctx,
+		"go", "build", "-ldflags=-s -w", "-v", "-o", name, pkg,
 	).With(
 		shx.WithEnv(map[string]string{
 			"CGO_ENABLED": "0",
 		}),
 	).Run()
+}
+
+// Build release binaries for each example app
+func (Build) Examples(ctx context.Context) error {
+	mg.CtxDeps(ctx,
+		mg.F(Build{}.release, "./examples/gdev", "./gdev"),
+		mg.F(Build{}.release, "./examples/custom-commands", "./example-custom-commands"),
+		mg.F(Build{}.release, "./examples/stack", "./example-stack"),
+	)
+	return nil
 }
