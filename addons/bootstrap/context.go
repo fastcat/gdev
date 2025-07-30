904aba810a90285254410f2414fc51c4ab6c95fc
Matthew Gabeler-Lee <cheetah@fastcat.org>
2025-07-30 03:07:53 -0400

    fix: relocate some types to avoid dependency loops in the future

--- addons/bootstrap/context.go
+++ addons/bootstrap/context.go
@@ -2,58 +2,29 @@ package bootstrap
 
 import (
 	"context"
-	"fmt"
-	"reflect"
+
+	"fastcat.org/go/gdev/addons/bootstrap/internal"
 )
 
-type Context struct {
-	context.Context
-	info map[infoKey]any
-}
+// aliases exposing a limited subset of the internal Context API
+
+type (
+	Context        = internal.Context
+	InfoKey[T any] = internal.InfoKey[T]
+)
 
 func NewContext(ctx context.Context) *Context {
-	return &Context{
-		Context: ctx,
-		info:    map[infoKey]any{},
-	}
-}
-
-type InfoKey[T any] struct {
-	k string
-	_ [0]*T // make keys for different types non-convertible
-}
-
-func (k InfoKey[T]) key() string       { return k.k }
-func (k InfoKey[T]) typ() reflect.Type { return reflect.TypeFor[T]() }
-
-type infoKey interface {
-	key() string
-	typ() reflect.Type
+	return internal.NewContext(ctx)
 }
 
 func NewKey[T any](name string) InfoKey[T] {
-	return InfoKey[T]{k: name}
+	return internal.NewKey[T](name)
 }
 
 func Save[T any](ctx *Context, k InfoKey[T], v T) {
-	if _, ok := ctx.info[k]; ok {
-		panic(fmt.Errorf("already saved %s for %v", k.k, k.typ()))
-	}
-	ctx.info[k] = v
+	internal.Save(ctx, k, v)
 }
 
 func Get[T any](ctx *Context, k InfoKey[T]) (T, bool) {
-	v, ok := ctx.info[k]
-	if !ok {
-		var t T
-		return t, ok
-	}
-	return v.(T), ok
-}
-
-func (ctx *Context) Value(key any) any {
-	if k, ok := key.(infoKey); ok {
-		return ctx.info[k]
-	}
-	return ctx.Context.Value(key)
+	return internal.Get(ctx, k)
 }
---
+++ addons/bootstrap/internal/doc.go
@@ -0,0 +1,6 @@
+// Package internal provides internal types and functions for the bootstrap
+// addon.
+//
+// Most of these are here to avoid dependency cycles, and will have aliases for
+// most of their functionality exposed in the main bootstrap package.
+package internal
