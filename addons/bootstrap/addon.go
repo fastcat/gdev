836a157c2fff0fa21e048bec4f4f52a5890301d6
Matthew Gabeler-Lee <cheetah@fastcat.org>
2025-08-15 09:17:23 -0400

    chore: move func to better place

--- addons/bootstrap/addon.go
+++ addons/bootstrap/addon.go
@@ -133,21 +133,3 @@ func WithAlternatePlanCmd(
 		return cmd
 	})
 }
-
-const defaultCmdShort = "install & configure system dependencies"
-
-func runnerCmd(plan *Plan) *cobra.Command {
-	dryRun := false
-	cmd := &cobra.Command{
-		Args:  cobra.NoArgs,
-		Short: defaultCmdShort,
-		RunE: func(cmd *cobra.Command, _ []string) error {
-			if dryRun {
-				return SimPlan(cmd.Context(), plan)
-			}
-			return RunPlan(cmd.Context(), plan)
-		},
-	}
-	cmd.Flags().BoolVarP(&dryRun, "dry-run", "n", dryRun, "don't actually change anything")
-	return cmd
-}
--- addons/bootstrap/cmd.go
+++ addons/bootstrap/cmd.go
@@ -1,6 +1,28 @@
 package bootstrap
 
-import "context"
+import (
+	"context"
+
+	"github.com/spf13/cobra"
+)
+
+const defaultCmdShort = "install & configure system dependencies"
+
+func runnerCmd(plan *Plan) *cobra.Command {
+	dryRun := false
+	cmd := &cobra.Command{
+		Args:  cobra.NoArgs,
+		Short: defaultCmdShort,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			if dryRun {
+				return SimPlan(cmd.Context(), plan)
+			}
+			return RunPlan(cmd.Context(), plan)
+		},
+	}
+	cmd.Flags().BoolVarP(&dryRun, "dry-run", "n", dryRun, "don't actually change anything")
+	return cmd
+}
 
 func RunPlan(ctx context.Context, plan *Plan) error {
 	plan.AddDefaultSteps()
