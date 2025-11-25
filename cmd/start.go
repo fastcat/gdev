9b50c1af8cce6af97c89445a23f9a582f5cb7e79
Matthew Gabeler-Lee <cheetah@fastcat.org>
2025-11-25 18:34:12 -0500

    fix: allow adding custom flags to the stack start command

--- cmd/start.go
+++ cmd/start.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 
 	"fastcat.org/go/gdev/instance"
 	"fastcat.org/go/gdev/service"
@@ -9,13 +10,28 @@ import (
 )
 
 func init() {
-	instance.AddCommands(&cobra.Command{
-		Use:   "start",
-		Short: "start the stack",
-		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			// TODO: use go-pretty/v6/progress
-			return stack.Start(cmd.Context(), service.WithServiceModes(service.ConfiguredModes()))
-		},
+	instance.AddCommandBuilders(func() *cobra.Command {
+		instance.CheckLockedDown()
+		cmd := &cobra.Command{
+			Use:   "start",
+			Short: "start the stack",
+			Args:  cobra.NoArgs,
+			RunE: func(cmd *cobra.Command, args []string) error {
+				// TODO: use go-pretty/v6/progress
+				return stack.Start(cmd.Context(), service.WithServiceModes(service.ConfiguredModes()))
+			},
+		}
+		f := cmd.Flags()
+		for _, fn := range startFlaggers {
+			fn(f)
+		}
+		return cmd
 	})
+}
+
+var startFlaggers []func(*pflag.FlagSet)
+
+func AddStartFlaggers(fns ...func(*pflag.FlagSet)) {
+	instance.CheckCanCustomize()
+	startFlaggers = append(startFlaggers, fns...)
 }
