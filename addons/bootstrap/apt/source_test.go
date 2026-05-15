24c7495dc26655747aeacc9a19881dda9ae180be
Matthew Gabeler-Lee <cheetah@fastcat.org>
2026-05-15 17:04:50 -0400

    fix: update test

--- addons/bootstrap/apt/source_test.go
+++ addons/bootstrap/apt/source_test.go
@@ -56,6 +56,7 @@ func TestAptSource_ToDeb822(t *testing.T) {
 				"Suites":     "trixie",
 				"Components": "main non-free non-free-firmware contrib",
 				"Signed-By":  "/usr/share/keyrings/debian-archive-keyring.gpg",
+				"Enabled":    "yes",
 			},
 		},
 	}
